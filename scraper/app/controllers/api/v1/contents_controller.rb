4669a53d353660d56b489e1897391ffd130f964b
John Nenniger <JPNENNIGER@GMAIL.com>
2017-04-01 16:25:12 -0700

    started writing api

--- scraper/app/controllers/api/v1/contents_controller.rb
+++ scraper/app/controllers/api/v1/contents_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::ContentsController < ApplicationController
 
-  def index
-    
+  
+
+  def compare
+    highlighted = "This is a test"
+    @sentences = Content.sentence.order(created_at: :desc)
+    @sentences.each do |sentence|
+      if sentence == highlighted
+        Content.find
+    end
   end
 
 end
