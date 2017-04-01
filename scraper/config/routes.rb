c4f446d73c55f78d9fb0e45a19240b0c925078ad
John Nenniger <JPNENNIGER@GMAIL.com>
2017-03-31 19:10:22 -0700

    created api controller

--- scraper/config/routes.rb
+++ scraper/config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  root 'contents#create'
- # root 'articles#create'
+ root 'articles#create'
 
- resources :contents
+ namespace :api, defaults: {format: :json} do
+   namespace :v1 do
+     resources :contents, only: [:index]
+   end
+ end
+
+ # resources :contents
 
 end
