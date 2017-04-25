cf7a3cc8f3d3f716003480065b02b6688e52670f
John Nenniger <JPNENNIGER@GMAIL.com>
2017-04-24 17:23:44 -0700

    comented out scraper.rb since it seems to be causing problems. it isn't needed in production mode

--- scraper/app/services/scraper.rb
+++ scraper/app/services/scraper.rb
@@ -8,26 +8,26 @@ require 'Pry'
 require 'Nokogiri'
 # def cbc_article(url)
   # base_url = url
-  base_url = "http://www.cbc.ca/radio/asithappens/as-it-happens-wednesday-edition-1.4045787/create-a-carrot-that-evokes-larger-social-issues-artist-makes-whatever-her-algorithm-demands-1.4046430?cmp=rss"
-
-  page = HTTParty.get(base_url);
-
-  parse_page = Nokogiri::HTML(page);
-
-  title = parse_page.css('.story-title').text
-  if title == ""
-    title = parse_page.css('.segment-headline').text
-  end
-  Pry.start(binding)
-
-  begin
-    Date.parse(parse_page.css('.delimited').text)
-  rescue
-    date = Date.new(2000,1,1)
-  end
+  # base_url = "http://www.cbc.ca/radio/asithappens/as-it-happens-wednesday-edition-1.4045787/create-a-carrot-that-evokes-larger-social-issues-artist-makes-whatever-her-algorithm-demands-1.4046430?cmp=rss"
   #
-  # date = Date.parse(parse_page.css('.delimited').text)
-  puts title
-  puts date
-  return [title, date]
+  # page = HTTParty.get(base_url);
+  #
+  # parse_page = Nokogiri::HTML(page);
+  #
+  # title = parse_page.css('.story-title').text
+  # if title == ""
+  #   title = parse_page.css('.segment-headline').text
+  # end
+  # Pry.start(binding)
+  #
+  # begin
+  #   Date.parse(parse_page.css('.delimited').text)
+  # rescue
+  #   date = Date.new(2000,1,1)
+  # end
+  # #
+  # # date = Date.parse(parse_page.css('.delimited').text)
+  # puts title
+  # puts date
+  # return [title, date]
 # end
